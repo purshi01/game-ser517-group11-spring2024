4cfcd6a724d818f06b94f5514501854476616d7e
Pete711 <158525501+pete-RK@users.noreply.github.com>
2024-02-06 12:11:53 -0800

    Adding react functionality to sign up form

--- src/LoginRegistration/SignUp.jsx
+++ src/LoginRegistration/SignUp.jsx
@@ -1,33 +1,37 @@
-import React from 'react'
-import '../styles/Login.css'
+import React, { useState } from 'react'
+import '../styles/SignUp.css'
 
 import profile_icon from '../assets/person.png'
 import email_icon from '../assets/email.png'
 import password_icon from '../assets/password.png'
 
 export const Login = () => {
+    const [action, setAction] = useState("Sign Up");
+
     return (
         <div className='container'>
             <div className='header1'>
-                <div className='text'>Sign Up</div>
+                <div className='text'>{action}</div>
                 <div className='underline'></div>
             </div>
-            <div className='input'>
-                <img src={profile_icon} alt=""/>
-                <input type = "text" placeholder='User Name'/>
-            </div>
-            <div className='input'>
-                <img src={email_icon} alt=""/>
-                <input type = "text" placeholder='User Email'/>
-            </div>
-            <div className='input'>
-                <img src={password_icon} alt=""/>
-                <input type = "text" placeholder='Password'/>
+            <div className="inputs">
+                <div className='input'>
+                    <img src={profile_icon} alt=""/>
+                    <input type = "text" placeholder='User Name'/>
+                </div>
+                <div className='input'>
+                    <img src={email_icon} alt=""/>
+                    <input type = "text" placeholder='User Email'/>
+                </div>
+                <div className='input'>
+                    <img src={password_icon} alt=""/>
+                    <input type = "text" placeholder='Password'/>
+                </div>
             </div>
             <div className="forgot-password">Forgot Password? <span>Click Here</span></div>
             <div className="submit-container">
-                <div className="submit">Register</div>
-                <div className="submit">Login</div>
+                <div className={action==="Login"?"submit blank":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                <div className={action==="Sign Up"?"submit blank":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
             </div>
         </div>
     )
