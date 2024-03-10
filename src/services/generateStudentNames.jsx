960dacd562f7f8b80d4d77db7d96c9db0633a9a2
Tahir Pervez <tpervez@asu.edu>
2024-03-10 13:19:28 -0700

    working at figuring out how to actually test with database

--- src/services/generateStudentNames.jsx
+++ src/services/generateStudentNames.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-const generateRandomName = async () => {
+export const generateRandomName = async () => { 
     const color = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet', 'Black', 'White', 'Pink', 'Purple'];
     const object = ['Whale', 'Fish', 'Dog', 'Cat', 'Mouse', 'Rat', 'Frog', 'Horse', 'Spaceship', 'Car', 'Bike', 'Bus', 'Train', 'Ship', 'Boat', 'Shuttle', 'Plane', 'Jet', 'King', 'Queen', 'Jester', 'Joker', 'Ace', 'Crown', 'Prince', 'Princess', 'Knight', 'Rook', 'Pawn', 'Bishop', 'Cheese', 'Pasta', 'Noodles', 'Soup', 'Egg', 'Carrot', 'Apple', 'Banana', 'Guitar', 'Piano', 'Violin', 'Organ', 'Keyboard', 'Square', 'Triangle', 'Circle', 'Rectangle', 'Lake', 'Sea', 'Ocean', 'Pond', 'Water', 'Rain', 'Cloud'];
     
@@ -16,11 +16,12 @@ const generateRandomName = async () => {
         studentName = randomColor + randomObject + randomNumber;
        
         try {
-            const response = await axios.get(`${API_BASE_URL}/check/${studentName}`);
+            const response = await axios.get(`${API_BASE_URL}/${studentName}`);
             existsInDatabase = response.data.exists;
         } catch (error) {
             console.error("Error checking if student name exists:", error);
             existsInDatabase = false; // Assuming it doesn't exist if there's an error
+            throw error;
         }
         console.log(studentName);
     } while (existsInDatabase)
