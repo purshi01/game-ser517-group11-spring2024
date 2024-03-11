436e711b98c23b1988d23bd2d4f9223c28da2628
Tahir Pervez <tpervez@asu.edu>
2024-03-11 16:16:20 -0700

    added password generation

--- src/Pages/NameGenerator.jsx
+++ src/Pages/NameGenerator.jsx
@@ -5,22 +5,27 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 const NameGenerator = () => {
   const [generatedNames, setGeneratedNames] = useState('');
-  const [numOfStudents, setNumOfStudents] = useState('');
+  const [numOfStudents, setNumOfStudents] = useState('1');
 
   const generateRandomName = () => { 
     const color = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet', 'Black', 'White', 'Pink', 'Purple'];
-    const object = ['Whale', 'Fish', 'Dog', 'Cat', 'Mouse', 'Rat', 'Frog', 'Horse', 'Spaceship', 'Car', 'Bike', 'Bus', 'Train', 'Ship', 'Boat', 'Shuttle', 'Plane', 'Jet', 'King', 'Queen', 'Jester', 'Joker', 'Ace', 'Crown', 'Prince', 'Princess', 'Knight', 'Rook', 'Pawn', 'Bishop', 'Cheese', 'Pasta', 'Noodles', 'Soup', 'Egg', 'Carrot', 'Apple', 'Banana', 'Guitar', 'Piano', 'Violin', 'Organ', 'Keyboard', 'Square', 'Triangle', 'Circle', 'Rectangle', 'Lake', 'Sea', 'Ocean', 'Pond', 'Water', 'Rain', 'Cloud'];
-    
+    const object = ['Whale', 'Fish', 'Dog', 'Cat', 'Mouse', 'Sheep', 'Goat', 'Rat', 'Frog', 'Horse', 'Spaceship', 'Car', 'Bike', 'Bus', 'Train', 'Ship', 'Boat', 'Shuttle', 'Plane', 'Jet', 'King', 'Queen', 'Jester', 'Joker', 'Ace', 'Crown', 'Prince', 'Princess', 'Knight', 'Rook', 'Pawn', 'Bishop', 'Cheese', 'Pasta', 'Noodles', 'Soup', 'Egg', 'Carrot', 'Apple', 'Banana', 'Guitar', 'Piano', 'Violin', 'Organ', 'Keyboard', 'Square', 'Triangle', 'Circle', 'Rectangle', 'Lake', 'Sea', 'Ocean', 'Pond', 'Water', 'Rain', 'Cloud'];
+    const letters = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+
     let studentName;
     let existsInDatabase;
+    let password = '';
 
     do {
         let randomColor = color[Math.floor(Math.random()*color.length)];
         let randomObject = object[Math.floor(Math.random()*object.length)];
         let randomNumber = Math.floor(Math.random()*900) + 100;
+        for (let i = 0; i < 20; i++) {
+          password += letters[Math.floor(Math.random() * letters.length)];
+        }
+
         studentName = randomColor + randomObject + randomNumber;
-        
-        existsInDatabase = false;
+
        /*
         try {
             // const response = await axios.get(`${API_BASE_URL}/${studentName}`);
@@ -33,25 +38,22 @@ const NameGenerator = () => {
         }
         */
 
-        
-    } while (existsInDatabase)
-    return studentName;
+    } while (generatedNames.includes(studentName) || existsInDatabase)
+    return {username: studentName, password: password};
   };
-  const addNStudents = () => {
+
+  const addStudents = () => {
     const num = parseInt(numOfStudents);
     if (!isNaN(num) && num > 0) {
-      const newNames = [];
+      const newStudents = [];
       for (let i = 0; i < num; i++) {
-        let newName;
-        do {
-          newName = generateRandomName();
-        } while (newNames.includes(newName));
-        newNames.push(newName);
+        let newStudent = generateRandomName();
+        newStudents.push(newStudent.username + ',' + newStudent.password);
       }
-      if(generatedNames == '') {
-        setGeneratedNames(newNames.join('\n'))
+      if(generatedNames === '') {
+        setGeneratedNames(newStudents.join('\n'))
       } else {
-        setGeneratedNames(generatedNames + '\n' + newNames.join('\n'));
+        setGeneratedNames(prevNames => prevNames + '\n' + newStudents.join('\n'));
       }
     }
   };
@@ -61,7 +63,7 @@ const NameGenerator = () => {
       <h1>Testing random name generation</h1>
       <textarea value={generatedNames} readOnly cols={50} rows={15}> </textarea>
       <textarea value={numOfStudents} typeof="number" onChange={(e) => setNumOfStudents(e.target.value)}></textarea>
-      <button onClick={addNStudents}>Generate Multiple Students</button>
+      <button onClick={addStudents}>Generate Multiple Students</button>
     </div>
   );
 };
