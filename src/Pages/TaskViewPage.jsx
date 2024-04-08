db711f57cff0bb1268771cfbf3bef6dcd3ad4f99
Tahir Pervez <tpervez@asu.edu>
2024-04-08 12:55:10 -0700

    setting but communication with backend

--- src/Pages/TaskViewPage.jsx
+++ src/Pages/TaskViewPage.jsx
@@ -3,8 +3,10 @@ import axios from "axios";
 import "../styles/TaskView.css"
 import { useParams } from "react-router-dom";
 import TasksComponent from "../components/common/TasksComponent";
+import Papa from "papaparse";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 
 const TaskView = () => {
   useEffect(() => {
@@ -29,6 +31,7 @@ const TaskView = () => {
   const {courseId, taskId} = useParams();
   const [taskListData, setTaskListData] = useState([]);
   const [file, setFile] = useState(null);
+  const [grades, setGrades] = useState([]);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -36,7 +39,7 @@ const TaskView = () => {
       if (file.name.endsWith('.csv') || file.name.endsWith('.xlsx') || file.name.endsWith('.xls')) {
         setFile(file);
       } else {
-        alert('File must be a .csv, .xls, or .xlsx');
+        alert('File must be a CSV or Excel file (.csv, .xls, or .xlsx)');
       }
     }
   };
@@ -44,10 +47,36 @@ const TaskView = () => {
   const handleUpload = () => {
     if (file === null) {
         alert('Please select a .csv, .xls, or .xlsx file.');
-    } else if (file.name.endsWith('csv')) {
-        console.log('Uploaded CSV file:', file);
-    } else if (file.name.endsWith('.xlsx') || file.name.endsWith('.xls')) {
-        console.log('Uploaded Excel file', file);
+    } else if (file.name.endsWith('.csv')) {
+        console.log("Reading in CSV", file);
+        Papa.parse(file, {
+            skipEmptyLines:true,
+            complete: function (results) {
+                console.log(results.data)
+                results.data.forEach(async submission => {
+                    try {
+                        const username = submission[0];
+                        const score = submission[1];
+        
+                        const response1 = await axios.get() // Not sure here, need to send database username and get student id
+                        console.log(response1.data);
+                        const id = response1.data.studentId;
+                        const data = {
+                            username: username,
+                        }
+                        const response = await axios.post(`/courses/${courseId}/tasks/${taskId}/assign`, )
+                        
+
+                      } catch (error) {
+                        console.error('Error adding student to course:', error);
+                      }
+                });
+            },
+          });
+        
+        } else if (file.name.endsWith('.xlsx') || file.name.endsWith('.xls')) {
+        console.log("Reading in Excel", file);
+
     } else {
         alert('Please select a .csv, .xls, or .xlsx file.');
     }
@@ -68,6 +97,14 @@ const TaskView = () => {
         </div>
         <div className="column">
             <h2>This column is should show all grades in the current task</h2>
+            <div className="column">
+            <textarea className="generated-names" 
+            value={grades} 
+            readOnly 
+            cols={50} rows={15}
+            >{API_BASE_URL}
+          </textarea>
+        </div>
         </div>
       </div>
     </div>
