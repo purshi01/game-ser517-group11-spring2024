3fc0de28bac21bf5c73909fd390dea8fae151596
purushothamas <purushothamapurshi8@gmail.com>
2024-04-18 00:31:10 -0700

    Created a cart  and itemCard  component

---
+++ src/components/common/Cart.jsx
@@ -0,0 +1,31 @@
+const Cart = ({ items, onRemoveFromCart, onCheckout }) => {
+  const totalPoints = items.reduce(
+    (sum, item) => sum + item.points * item.quantity,
+    0
+  );
+
+  return (
+    <div className="shopping-cart">
+      <h2 className="cart-title">Your Cart</h2>
+      {items.map((item, index) => (
+        <div className="cart-item" key={index}>
+          <span className="cart-item-details">
+            {item.item_name} - {item.quantity} x {item.points} Points ={" "}
+            {item.quantity * item.points} Points
+          </span>
+          <button
+            className="action-button"
+            onClick={() => onRemoveFromCart(item.item_id)}
+          >
+            Remove
+          </button>
+        </div>
+      ))}
+      <div className="cart-total">Total Points: {totalPoints}</div>
+      <button className="action-button" onClick={onCheckout}>
+        Checkout
+      </button>
+    </div>
+  );
+};
+export default Cart;
